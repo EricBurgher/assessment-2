2e6d738f901a7b3e3a2355f5de0cbc4b467bc09f
Eric Burgher <ericburgher@Erics-Air.attlocal.net>
2021-06-11 16:44:44 -0500

    Assessment 2

--- data/classes.py
+++ data/classes.py
@@ -28,6 +28,7 @@ class Inventory:
         self.copies_available = copies_available
 
     @classmethod
+    # Do harder Googling on why this won't work.
     def current_inventory(cls):
         inventory_list = {}
         my_path = os.path.abspath(os.path.dirname(__file__))
@@ -38,3 +39,73 @@ class Inventory:
                 inventory_list[row[0]] = row[1:]
 
         return inventory_list
+
+class App:
+    def __init__(self, name, users):
+        self.name = name
+        self.users = Customers.customer_objects()
+        self.inventory = Inventory.current_inventory()
+
+    # def list_users(self):
+    #     print('\n')
+    #     for i, user in enumerate(self.users):
+    #         print(f'{i + 1}. {user.name} {user.email_address}')
+
+    def current_inventory(self):
+    # Do harder Googling on why this won't work.
+        current_inventory = {}
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/inventory.csv")
+        with open(path, 'r') as data_file:
+            reader = csv.DictReader(data_file)
+            for row in reader:
+                print(row)
+
+    def customer_data(drivers_num):
+    # Write logic on current list of video rentals by title only.
+        for user in self.users:
+            if drivers_num == Customers.dl_number:
+                return user
+        return "User Not Found"
+
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    path = os.path.join(my_path, "../data/inventory.csv")
+    with open(path, 'r') as data_file:
+        reader = csv.DictReader(data_file)
+        for row in reader:
+            print(row)
+        
+
+    def rent_video(self, drivers_id):
+        for id in self.id:
+            if drivers_id == Customers.dl.number:
+                video_rental = input("What video would you like to rent?: ")
+    # Not append. Decremement copies_available in CSV file. Look it up.
+                self.title[f'{title}'].append(video_rental)
+    # Add logic to remove 1 from copies_available in inventory.csv
+    # Add logic so that you can't rent less than 0 copies.
+    # If copies are at 0, display "Ope! There are no copies available right now."
+    # Add logic so that customer can't rent more than 3 copies at a time.
+    # If trying to rent 4 or more videos, display "I'm sorry, you already have 3 rentals out."
+    
+        self.save_title()
+
+    def return_video(self):
+        for id in self.id:
+            if drivers_id == Customers.dl.number:
+                video_return = input("What video are you returning today?: ")
+                self.title[f'{title}'].append(video_return)
+         # write logic to remove title from current_video_rentals for that customer
+         # write logic to add title back to inventory CSV
+         # once successful, populate string "Thank you for returning {title}. Come again soon."
+        self.save_title()
+
+    def add_user(self, user_data):
+        self.users.append(Customers(user_data['dl_number'],user_data['first_name'], user_data['last_name'], user_data['current_video_rentals']))
+        self.save()
+
+        my_path = os.path.abspath(os.path.dirname(__file__))
+        path = os.path.join(my_path, "../data/customers.csv")
+        with open(path, 'a') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(self.users[user_data['dl_number']])
--- runner.py
+++ runner.py
@@ -1,33 +1,32 @@
 # Write your solution here!
 import csv
-from data.app import App
+from data.classes import App
 from data.classes import Customers
 from data.classes import Inventory
 
-app = App("Video Store Interface")
 mode = ''
-
 
 while mode != '6':
     mode = input("\nWelcome to Code Platoon Video!\nOptions:\n1. View video inventory \n2. View customer's rented videos \n3. Rent video \n4. Return video \n5. Add new customer\n6. Exit\n\nWhat would you like to do?: ")
+    # I don't understand why this isn't working. Moving on...
     if mode == '1':
         Inventory.current_inventory()
     elif mode == '2':
         drivers_id = input("Enter driver's license number: ")
-        app.customer_data()
+        App.customer_data()
     elif mode == '3':
         drivers_id = input("Enter driver's license number: ")
-        app.rent_video(drivers_id)
+        App.rent_video(drivers_id)
     elif mode == "4":
         drivers_id = input("Enter driver's license number: ")
-        app.return_video(drivers_id)
+        App.return_video(drivers_id)
     elif mode == "5":
         user_data = {}
         user_data['dl_number'] = input("Enter your driver's license number: ")
         user_data['first_name'] = input("Enter your first name: ")
         user_data['last_name'] = input("Enter your last name: ")
         user_data['current_video_rentals'] = input("What video would you like to rent today? ")
-        app.add_user(user_data)
+        App.add_user(user_data)
 
     elif mode == '6':
         break
